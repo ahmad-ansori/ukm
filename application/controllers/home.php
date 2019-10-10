3d052b290370c6395045ba0144fc51fb91d19e80
ahmadansori510@gmail.com <ahmadansori510@gmail.com>
2019-10-10 20:06:57 +0700

    ukm

---
+++ application/controllers/home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home extends CI_Controller {
+
+	public function _construct(){
+		parent::_construct();
+		$this->load->helper('url');
+	}
+
+
+	public function index()
+	{
+		$this->load->view('ukm');
+	}
+}
